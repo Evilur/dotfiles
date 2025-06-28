922e05212fbda24fe9e5d720f67ec13828bba282
Andrew Shershunov <mail@evilur.ru>
2025-06-28 23:10:19 +0300

    feat: Setting up second monitor

--- config/nvim/lua/plugins/night-owls-song.lua
+++ config/nvim/lua/plugins/night-owls-song.lua
@@ -1,8 +1,10 @@
 return {
     'Evilur/NightRider.nvim',
     lazy = false,
+    dev = true,
+    dir = '~/Projects/NightOwlSong.nvim',
     priority = 1000,
     config = function()
-        vim.cmd('colorscheme evilur')
+        vim.cmd('colorscheme night-owls-song')
     end
 }
