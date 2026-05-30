f1df8a47f4a4feff99de5994dca88f4dc9c8be93
Andrew Shershunov <the.evilur@gmail.com>
2026-05-31 01:22:26 +0300

    feat(nvim): Add bufferline.nvim theme

--- config/nvim/lua/plugins/bufferline.lua
+++ config/nvim/lua/plugins/bufferline.lua
@@ -3,25 +3,17 @@ return {
     lazy = false,
     dependencies = 'nvim-tree/nvim-web-devicons',
     config = function()
-        -- Get the colors --
-        local mantle = {
-            attribute = 'bg',
-            highlight = 'Pmenu'
-        }
-        local crust = {
-            attribute = 'bg',
-            highlight = 'PmenuThumb'
-        }
-
-        -- Apply the config --
         local bufferline = require('bufferline')
+        local c = {
+            mantle  = '#1e2030',
+            grey_15 = '#3d4262',
+            text    = '#f4f2fd',
+            red     = '#ff4064'
+        }
         bufferline.setup {
             options = {
-                hover = { enabled = true },
-                style_preset = {
-                    bufferline.style_preset.no_italic,
-                    bufferline.style_preset.no_bold,
-                },
+                hover = { enabled = false },
+                style_preset = 'minimal',
                 separator_style = 'thin',
                 left_mouse_command = nil,
                 right_mouse_command = nil,
@@ -30,6 +22,244 @@ return {
                 close_icon = '',
                 modified_icon = '●',
                 close_command = 'Bdelete %d',
+            },
+            highlights = {
+                fill = {
+                    fg = c.text,
+                    bg = c.mantle
+                },
+                background = {
+                    fg = c.text,
+                    bg = c.mantle
+                },
+                tab = {
+                    fg = c.text,
+                    bg = c.mantle
+                },
+                tab_selected = {
+                    fg = c.text,
+                    bg = c.mantle
+                },
+                tab_separator = {
+                    fg = c.text,
+                    bg = c.mantle
+                },
+                tab_separator_selected = {
+                    fg = c.text,
+                    bg = c.mantle
+                },
+                tab_close = {
+                    fg = c.text,
+                    bg = c.mantle
+                },
+                close_button = {
+                    fg = c.text,
+                    bg = c.mantle
+                },
+                close_button_visible = {
+                    fg = c.text,
+                    bg = c.mantle
+                },
+                close_button_selected = {
+                    fg = c.text,
+                    bg = c.mantle
+                },
+                buffer_visible = {
+                    fg = c.text,
+                    bg = c.mantle
+                },
+                buffer_selected = {
+                    fg = c.text,
+                    bg = c.mantle,
+                    underline = true,
+                    sp = c.red,
+                    bold = true,
+                    italic = true,
+                },
+                numbers = {
+                    fg = c.text,
+                    bg = c.mantle
+                },
+                numbers_visible = {
+                    fg = c.text,
+                    bg = c.mantle
+                },
+                numbers_selected = {
+                    fg = c.text,
+                    bg = c.mantle
+                },
+                diagnostic = {
+                    fg = c.text,
+                    bg = c.mantle
+                },
+                diagnostic_visible = {
+                    fg = c.text,
+                    bg = c.mantle
+                },
+                diagnostic_selected = {
+                    fg = c.text,
+                    bg = c.mantle
+                },
+                hint = {
+                    fg = c.text,
+                    bg = c.mantle
+                },
+                hint_visible = {
+                    fg = c.text,
+                    bg = c.mantle
+                },
+                hint_selected = {
+                    fg = c.text,
+                    bg = c.mantle
+                },
+                hint_diagnostic = {
+                    fg = c.text,
+                    bg = c.mantle
+                },
+                hint_diagnostic_visible = {
+                    fg = c.text,
+                    bg = c.mantle
+                },
+                hint_diagnostic_selected = {
+                    fg = c.text,
+                    bg = c.mantle
+                },
+                info = {
+                    fg = c.text,
+                    bg = c.mantle
+                },
+                info_visible = {
+                    fg = c.text,
+                    bg = c.mantle
+                },
+                info_selected = {
+                    fg = c.text,
+                    bg = c.mantle
+                },
+                info_diagnostic = {
+                    fg = c.text,
+                    bg = c.mantle
+                },
+                info_diagnostic_visible = {
+                    fg = c.text,
+                    bg = c.mantle
+                },
+                info_diagnostic_selected = {
+                    fg = c.text,
+                    bg = c.mantle
+                },
+                warning = {
+                    fg = c.text,
+                    bg = c.mantle
+                },
+                warning_visible = {
+                    fg = c.text,
+                    bg = c.mantle
+                },
+                warning_selected = {
+                    fg = c.text,
+                    bg = c.mantle
+                },
+                warning_diagnostic = {
+                    fg = c.text,
+                    bg = c.mantle
+                },
+                warning_diagnostic_visible = {
+                    fg = c.text,
+                    bg = c.mantle
+                },
+                warning_diagnostic_selected = {
+                    fg = c.text,
+                    bg = c.mantle
+                },
+                error = {
+                    fg = c.text,
+                    bg = c.mantle
+                },
+                error_visible = {
+                    fg = c.text,
+                    bg = c.mantle
+                },
+                error_selected = {
+                    fg = c.text,
+                    bg = c.mantle
+                },
+                error_diagnostic = {
+                    fg = c.text,
+                    bg = c.mantle
+                },
+                error_diagnostic_visible = {
+                    fg = c.text,
+                    bg = c.mantle
+                },
+                error_diagnostic_selected = {
+                    fg = c.text,
+                    bg = c.mantle
+                },
+                modified = {
+                    fg = c.text,
+                    bg = c.mantle
+                },
+                modified_visible = {
+                    fg = c.text,
+                    bg = c.mantle
+                },
+                modified_selected = {
+                    fg = c.text,
+                    bg = c.mantle
+                },
+                duplicate_selected = {
+                    fg = c.text,
+                    bg = c.mantle
+                },
+                duplicate_visible = {
+                    fg = c.text,
+                    bg = c.mantle
+                },
+                duplicate = {
+                    fg = c.text,
+                    bg = c.mantle
+                },
+                separator_selected = {
+                    fg = c.mantle,
+                    bg = c.mantle
+                },
+                separator_visible = {
+                    fg = c.mantle,
+                    bg = c.mantle
+                },
+                separator = {
+                    fg = c.mantle,
+                    bg = c.mantle
+                },
+                indicator_visible = {
+                    fg = c.mantle,
+                    bg = c.mantle
+                },
+                indicator_selected = {
+                    fg = c.mantle,
+                    bg = c.mantle
+                },
+                pick_selected = {
+                    fg = c.mantle,
+                    bg = c.mantle
+                },
+                pick_visible = {
+                    fg = c.mantle,
+                    bg = c.mantle
+                },
+                pick = {
+                    fg = c.mantle,
+                    bg = c.mantle
+                },
+                offset_separator = {
+                    fg = c.mantle,
+                    bg = c.mantle
+                },
+                trunc_marker = {
+                    fg = c.mantle,
+                    bg = c.mantle
+                }
             }
         }
     end
